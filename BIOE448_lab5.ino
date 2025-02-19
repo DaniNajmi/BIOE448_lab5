82e46c51fbf89c71e81b0651e6f5c1447c5b05e9
DaniNajmi <dnajmiasl@gmail.com>
2025-02-19 14:18:08 -0600

    More changes

--- BIOE448_lab5.ino
+++ BIOE448_lab5.ino
@@ -2,50 +2,41 @@
 // February 19th, 2025
 // Daniela Najmias Lang, Matan Blitz, Marc De Guzman
 
+#include <ArduinoBLE.h>
+BLEService newService("180A");
+
+BLEByteCharacteristic readChar("2A58", BLERead);
+BLEByteCharacteristic writeChar("2A57", BLEWrite);
+
+long previousMillis = 0;
+
 const int trigPin = 8;
 const int echoPin = 10;
 long duration;
 int distanceCm, distanceInch;
-long previousMillis = 0;
-
-const int red = 2; // red LED
-const int green = 3; // green LED
-const int range = 5; // range for changing from green to red based on distance in cm
-
-// Including the libraries
-#include <ArduinoBLE.h>
-BLEService newService("180A"); // creating the service
-BLEByteCharacteristic readChar("2A57", BLERead);
-BLEByteCharacteristic writeChar("2A58", BLEWrite);
-
 
 void setup() {
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
-  pinMode(red, OUTPUT);
-  pinMode(green, OUTPUT);
   Serial.begin(9600);
 
-  // Naming the device
-  BLE.setLocalName("DanielaMatanMarc");
+  while(!Serial);
+  if (!BLE.begin()){
+    Serial.println("Waiting for ArduinoBLE");
+    while(1);
+  }
+
+  BLE.setDeviceName("DanielaMatanMarc");
   BLE.setAdvertisedService(newService);
   newService.addCharacteristic(readChar);
   newService.addCharacteristic(writeChar);
   BLE.addService(newService);
-
-  // Adding loop for Bluetooth
-  while(!Serial);
-  if (!BLE.begin()){
-  Serial.println("Waiting for ArduinoBLE");
-  while(1);
-  }
-
+  
   readChar.writeValue(0);
   writeChar.writeValue(0);
 
   BLE.advertise();
   Serial.println("Bluetooth device active");
-
 }
 
 void loop() {
@@ -78,7 +69,7 @@ void loop() {
         Serial.print(distanceCm);
         Serial.println(" cm");
 
-        delay(10);
+        delay(1000);
         readChar.writeValue(distanceCm);
         Serial.println("Distance printed to peripheral");
       }
